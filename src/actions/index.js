a18951a8cbd99be933e90081b999a446616d4423
Ruhaim Izmeth <ruhaim@gmail.com>
2018-03-02 09:40:04 +0530

    modified book events

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
-import { ADD_ARTICLE } from "../action-types/action-types.js";
+import {
+  ADD_BOOOK,
+  MODIFY_BOOK,
+  GET_BOOKS
+} from "../action-types/action-types.js";
 
-export const addArticle = article => ({
-  type: ADD_ARTICLE,
-  payload: article
+export const addBook = book => ({
+  type: ADD_BOOK,
+  payload: book
+});
+
+export const modifyBook = book => ({
+  type: MODIFY_BOOK,
+  payload: book
+});
+
+export const getBooks = books => ({
+  type: GET_BOOKS,
+  payload: books
 });
